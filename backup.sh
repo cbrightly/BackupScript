1d86469acd40697b17dac59388ebaaddf7a5207b
cbrightly <chris.brightly@gmail.com>
2015-09-24 21:33:59 -0400

    Added a few further sanity checks to destination directory selection and creation. Added header and further commenting for clarity

--- backup.sh
+++ backup.sh
@@ -1,25 +1,61 @@
 #!/bin/bash
-DATE=`date +%F` # Grab the date
-HOST=`hostname` # Grab the hostname
-NAME=`whoami` # Grab username
-BACKUPDIR=/backup # Set default destination in root dir
-if [[ $EUID -ne 0 ]]; then
-  echo "Not root. Backing up home directory only..."
-  DATA=$HOME
+
+# Written by:
+#  Chris Brightly <chris.brightly@gmail.com>
+#
+# Simple BASH script for backup which takes no arguments.
+#
+# If root user then backs up entire system to a tgz. If the
+# user is NOT root, then it creates a tgz of $HOME.
+# Regardless of username, the script attempts to backup
+# /var/www to a seperate tgz file.
+# If /backup exists and is writable 
+
+# Usage: sh backup.sh
+
+# Define variables here
+DATE=`date +%F`   # Grab the date
+HOST=`hostname`	  # Grab the hostname
+NAME=`whoami`	  # Grab username
+DESTINATION=$HOME/backup # Set default destination
+SOURCE=$HOME	  # Default source for direcrory to backup
+WEB=/var/www	  # web home
+
+# Check user & permissions, set src and dest accordingly
+if ! [ $(id -u) = 0 ]; then
+  echo "NOT running using root permissions. Backing up home directory only..."
+  SOURCE=$HOME
+  DESTINATION=$HOME/backup
  else
-  echo "root user! Backing up system..."
-  DATA=/
- fi
+  echo "Running as root user / with root  permissions! Backing up system..."
+  SOURCE=/
+  DESTINATION=/backup
+fi
+
+# Does backup destination exist?
+if [ ! -d $DESTINATION ]; then
+	echo "Backup destination doesn't exist!"
+	if [ mkdir $DESTINATION ]; then # Try to create destination if not existant
+		echo $DESTINATION " created..."
+	else
+		echo "Can't create " $DESTINATION ", aborting!"
+		exit 2
+	fi
+else
+	echo "Backing up to " $DESTINATION "..."
+fi
+
 # If destination isn't writable...
-  if [ ! -w $BACKUPDIR ] ; then
+if [ ! -w /backup ]; then
     echo "Cannot write to /backup; using ~/backup instead!"
-    BACKUPDIR=$HOME/backup
-  fi
-# Does /backup exist?
-  if [ ! -d "$BACKUPDIR" ]; then
-    mkdir $BACKUPDIR # Try to create destination if not existant
-  fi
-cd $BACKUPDIR
+    DESTINATION=$HOME/backup
+else
+    echo "Writing backup to " $DESTINATION "..."
+fi
+
+cd $DESTINATION
 TARFILE=$NAME-$DATE-$HOST.tgz # Use it to create a filename
-tar -czf $TARFILE $DATA --exclude $BACKUPDIR
-find $BACKUPDIR -name "*.tgz" -type f -mtime +3 -delete # Delete backups more than 3 days old
+tar -czf $TARFILE $SOURCE --exclude $DESTINATION
+TARFILE=$NAME-$DATE-$HOST-www.tgz # Create backup of www
+tar -czf $TARFILE $WEB --exclude $DESTINATION
+# find $DESTINATION -name "*.tgz" -type f -mtime +3 -delete # Delete backups more than 3 days old
