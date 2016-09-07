12af01f242d5e75ab639e4f249654444573c2c19
C Brightly <chris.brightly@gmail.com>
2016-09-07 17:45:13 -0400

    Correct fixed "/backup" directory in main script with $DEST variable

--- backup_basic.sh
+++ backup_basic.sh
@@ -20,24 +20,24 @@ SRC=/;				# Set SCP src
 DEST=/backup;			# Set SCP dest
 # Where is script running from?
 DIR=$( cd $(dirname $0) ; pwd -P );
-EXCLUDES="--exclude=/home/$USER/.bitcoin --exclude=/home/$USER/.litecoin --exclude=/home/$USER/backup";
+EXCLUDES="--exclude=/home/$USER/.bitcoin --exclude=/home/$USER/.litecoin --exclude=/home/$USER$DEST";
 
 echo "+==============================================+";
 echo "!!!  Working in ""$DIR"" !!!";
 echo "|%%%%    backup.sh: execution begining     %%%%|";
-sudo mkdir -p /backup;
-sudo tar -czf /backup/$DATE-$HOST-homes_backup.tar.gz /home $EXCLUDES
-echo "| + /home made into a tarball in /backup       |";
-sudo tar -czf /backup/$DATE-$HOST-www_backup.tar.gz /var/www/;
-echo "| + /var/www made into a tarball in /backup    |";
-sudo tar -czf /backup/$DATE-$HOST-etc_backup.tar.gz /etc;
-echo "| + /etc made into a tarball in /backup        |";
-sudo chown $USER /backup -R;
-echo "| + Changed ownership of /backup recursively   |";
+sudo mkdir -p $DEST;
+sudo tar -czf $DEST/$DATE-$HOST-homes_backup.tar.gz /home $EXCLUDES
+echo "| + /home made into a tarball in $DEST       |";
+sudo tar -czf $DEST/$DATE-$HOST-www_backup.tar.gz /var/www/;
+echo "| + /var/www made into a tarball in $DEST    |";
+sudo tar -czf $DEST/$DATE-$HOST-etc_backup.tar.gz /etc;
+echo "| + /etc made into a tarball in $DEST        |";
+sudo chown $USER $DEST -R;
+echo "| + Changed ownership of $DEST recursively   |";
 # Backup state of apt packages / settings / etc
 sudo bash $DIR/clone_packages.sh
 # Move apt-clone backup to backup destination
-mv $DIR/*apt*clone* $DEST
+mv $DIR/*apt*clone* $DEST/$DATE-$HOST-dpkglist.apt-clone.tar.gz
 echo "| + Backed up apt state/packages (apt-clone)   |";
 echo "|%%%%                                      %%%%|";
 echo "|%%%%     backup.sh execution complete     %%%%|";
