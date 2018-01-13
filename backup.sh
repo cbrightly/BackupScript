4295f8757ca3571fe2ee2cc1cf44ed6534ef5dfe
C. Brightly <chris.brightly@gmail.com>
2018-01-13 19:26:52 +0000

    Fix output of container tgz to bit bucket

--- backup.sh
+++ backup.sh
@@ -35,6 +35,7 @@ HOST=`hostname`		# Grab the hostname
 NAME=`whoami`		# Grab username
 DESTINATION=/backup	# Set default destination
 SOURCE=/		# Default dir to backup
+FILENAME=$DATE-$NAME_at_$HOST
 BACKUP_WWW=TRUE		# Backup $WEB + default data?
 WEB=/var/www		# web home
 RETAIN=3		# Days to keep existing backups
@@ -42,15 +43,22 @@ TEMP=/tmp
 #
 # !! You shouldn't need to change anything below here !!
 
+
+echo -e "--------------------"
+echo -e "BackupScript Running"
+echo -e "--------------------\n"
+
 # Check user & permissions, set src and dest accordingly
 if [ ! $(id -u) = 0 ]; then
-  echo -e "!!! NOT !!! running as root.\nAttempting backup of home directory only...\n"
   SOURCE=$HOME
   DESTINATION=$SOURCE/backup
+# echo -e "*** NOT running as root.\n  *** Only backing up user's home.\n"
+  echo -e "*** NOT RUNNING AS ROOT! ***\n^^^ This can limit the ability of this script to backup certain directories ^^^\n"
  else
-  echo -e "!!! Running as root!!!\nBacking up $SOURCE to $DESTINATION!\n"
   SOURCE=/
   DESTINATION=$SOURCE/backup
+  echo -e "!!! Running as root!!!\nBacking up:\n\t $SOURCE\n"
+  echo -e "Backup Destination:\n\t$DESTINATION\n"
 fi
 
 # Does backup destination exist?
@@ -58,10 +66,10 @@ if [ ! -d $DESTINATION ]; then
   # Non-existant backup destination!
   echo -e "Backup destination doesn't exist!\n"
   if [ $(mkdir $DESTINATION) ]; then # Try to create destination if not existant
-    echo -e "$DESTINATION created..."
+    echo -e "$DESTINATION created...\n"
   else
   # Can't create default backup dir
-    echo -e "Can't create $DESTINATION..."
+    echo -e "Can't create $DESTINATION...\n"
     if [ $(ls -d -- "$HOME/backup" > /dev/null 2>&1) ]; then
     # if [ -d $HOME/backup ]; then
       if [ -w $HOME/backup ]; then
@@ -94,23 +102,60 @@ fi
 
 cd $TEMP
 # Use variables to create a filename
-FILEMAIN=$TEMP/$NAME-$DATE-$HOST.tar.gz
-FILEWWW=$TEMP/$NAME-$DATE-$HOST-www.tar.gz
+FILEMAIN=$TEMP/$FILENAME.tgz
+FILEHOME=$TEMP/$FILENAME-home.tar.gz
+FILEWWW=$TEMP/$FILENAME-www.tar.gz
 
-echo -e "Creating main backup in file $FILEMAIN."
-tar -czf $FILEMAIN $SOURCE --exclude $DESTINATION --exclude $DESTINATION/.bitcoin > /dev/null 2>&1
+echo -e "Creating home backup:\n\t$FILEHOME"
+if tar -czf $FILEHOME $SOURCE --exclude $DESTINATION --exclude $DESTINATION/.bitcoin > /dev/null 2>&1; then
+  echo -e " ...okay!\n"
+else
+  echo -e " ...ERROR!\n"
+  exit
+fi
+
 # Create backup of $WWW
-echo -e "Creating web backup in file $FILEWWW."
-tar -czf $FILEWWW $WEB --exclude $DESTINATION --exclude $DESTINATION/.bitcoin > /dev/null 2>&1
+echo -e "Creating web backup:\n\t$FILEWWW"
+if tar -czf $FILEWWW $WEB --exclude $DESTINATION > /dev/null 2>&1; then
+  echo -e " ...okay!\n"
+else
+  echo -e " ...ERROR!\n"
+  exit
+fi
+
+# Create container tgz
+echo -e "Creating a container tgz file..."
+if tar -czf $FILEMAIN $FILEHOME $FILEWWW > /dev/null 2>&1 ; then
+  echo -e " ...okay!\n"
+else
+  echo -e " ...ERROR!\n"
+  exit
+fi
+
+# Remove temporary tar.gz files
+echo -e "Removing temporary tar.gz files..."
+if rm $FILEHOME $FILEWWW ; then
+  echo -e " ...okay!\n"
+else
+  echo -e " ...ERROR!\n"
+  exit
+fi
+
 # Move files from temp directory to final dir
-mv $FILEMAIN $FILEWWW $DESTINATION
+echo -e "Moving final tgz to backup destination..."
+if mv $FILEMAIN $DESTINATION; then
+  echo -e " ...okay!\n"
+else
+  echo -e " ...ERROR!\n"
+  exit
+fi
 
+# Go to destination directory
 cd $DESTINATION
-
 # Delete backups more than # days old
 # echo -e "Finding and deleting backup files older than $RETAIN days.."
 # find $DESTINATION -name "*.tgz" -type f -mtime +$RETAIN -delete
 # All done!
-echo -e "Success! Backup sources:  $WWW_ROOT and $SOURCE\nBackup(s) destination: $DESTINATION!\n"
+echo -e "Success!\n\tBackup sources:\n\t$WWW_ROOT\n\t$SOURCE\nBackup(s) destination:\n\t$DESTINATION!\n"
 # echo -e "Successfully purged pre-existing backup files older than $RETAIN days!\n"
 echo -e "*** Backup Script Completed! ***\n"
