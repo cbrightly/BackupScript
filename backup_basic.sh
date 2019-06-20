f620b6d14891ec7456ff3498fa62eb4c7af03d2a
C Brightly <chris.brightly@gmail.com>
2019-06-20 03:07:14 -0400

    fixed quotes around variables to prevent globbing or word splitting

--- backup_basic.sh
+++ backup_basic.sh
@@ -13,50 +13,57 @@
 #    which hold each blockchain, respectively.
 ########################################################
 
-DATE=`date +%F`;			# Grab the current date
-HOST=`hostname`;			# Grab the hostname
-USER=`whoami`;				# Get current username
-SRC=/;					# Set default BACKUP src
-DEST=/backup;				# Set default BACKUP dest
-CHOWNER=$USER:nogroup;			# Set default final owner+group
-DIR=$( cd $(dirname $0) ; pwd -P )	# Where is script running from?
+DATE=$(date +%F);				# Grab the current date
+HOST=$(hostname);				# Grab the hostname
+USER=$(whoami);					# Get current username
+SRC=/;						# Set default BACKUP src
+DEST=/backup;					# Set default BACKUP dest
+CHOWNER=$USER:nogroup;				# Set default final owner+group
+DIR=$( cd "$(dirname "$0")" || echo "Couldn't cd to $(dirname "$0")... quitting!" && exit ; pwd -P )	# Where is script running from?
+EXCLUDES="--exclude=$HOME/.bitcoin/* --exclude=$HOME/.litecoin/* --exclude=$HOME/backup/* --exclude=$HOME/.zcash/* --exclude=$HOME/.zcash-params/* --exclude=$HOME/archives/* --exclude=$HOME/*.com/* --exclude=$HOME/*.in/* --exclude=$HOME/*.tk/* --exclude=$HOME/.bitmonero/* --exclude=$HOME/www*";
 
-EXCLUDES="--exclude=/home/$USER/.bitcoin/* --exclude=/home/$USER/.litecoin/* --exclude=/home/$USER/backup/* --exclude=/home/$USER/.zcash/* --exclude=/home/$USER/.zcash-params/* --exclude=/home/$USER/archives/* --exclude=/home/$USER/*.com/* --exclude=/home/$USER/*.in/* --exclude=/home/$USER/*.tk/* --exclude=/home/$USER/.bitmonero/* --exclude=/home/$USER/www*";
 echo " ";
 echo "+==============================================+";
 echo "|  !! Backup Execution Begining...";
 # Create destination directory if not already existant
-  sudo if ![ -d $DEST ]; mkdir -p $DEST;
+# TODO: Need to check for write permission as well
+  if ! [ -d "$DEST" ]; then sudo mkdir -p "$DEST" && sudo chown "$CHOWNER" "$DEST"; fi;
 # Move to destination directory
-  cd $DEST;
-echo "|	 II  Changed current directory to $DEST";
+  cd "$DEST" || sudo mkdir -p "$DEST" #echo "Couldn't cd to $DEST... quitting!" && exit;
+  echo "|	 II  Changed current directory to $DEST";
 # Backup home folders
-  tar -czf $DATE-$HOST-homes_backup.tar.gz /home/$USER $EXCLUDES
-  echo "|	 II  /home made into a tarball in $DEST";
+  FILENAME="$DATE"-"$HOST"-home_backup.tar.gz;
+  tar -czf "$FILENAME" "$HOME" "$EXCLUDES";
+  echo "|	 II  $HOME made into a tarball in $DEST";
 # Backup www root
-  sudo tar -czf $DATE-$HOST-www_backup.tar.gz /var/www;
+  FILENAME="$DATE"-"$HOST"-www_backup.tar.gz;
+  sudo tar -czf "$FILENAME" /var/www;
   echo "|	 II  /var/www made into a tarball in $DEST";
 # Backup /etc
-  sudo tar -czf $DATE-$HOST-etc_backup.tar.gz /etc;
+  FILENAME="$DATE"-"$HOST"-etc_backup.tar.gz;
+  sudo tar -czf "$FILENAME" /etc;
   echo "|	 II  /etc made into a tarball in $DEST";
 # Backup apt files eight different ways
-  sudo tar -czf $DATE-$HOST-var_lib_dpkg.tar.gz /var/lib/dpkg;
+  FILENAME="$DATE"-"$HOST"-var_lib_dpkg.tar.gz;
+  sudo tar -czf "$FILENAME" /var/lib/dpkg;
   echo "|	 II  /var/lib/dpkg tarballed in $DEST";
-  sudo tar -czf $DATE-$HOST-apt_extended_states.tar.gz /var/lib/apt/extended_states;
+  FILENAME="$DATE"-"$HOST"-apt_extended_states.tar.gz;
+  sudo tar -czf "$FILENAME" /var/lib/apt/extended_states;
   echo "|	 II  /var/lib/apt/extended_states backed up";
-  sudo dpkg --get-selections "*" > $DEST/$DATE-$HOST-dpkg_get-selections.txt;
-  sudo chown $USER $DEST/$DATE-$HOST-dpkg_get-selections.txt;
-  sudo tar -czf $DATE-$HOST-dpkg_get-selections.tar.gz $DATE-$HOST-dpkg_get-selections.txt --remove-files
+  FILENAME="$DATE"-"$HOST"-dpkg_get-selections.txt;
+  sudo dpkg --get-selections "*" | sudo tee "$FILENAME";
+  sudo chown "$USER" "$DEST"/"$DATE"-"$HOST"-dpkg_get-selections.txt;
+  sudo tar -czf "$FILENAME" "$DATE"-"$HOST"-dpkg_get-selections.txt --remove-files;
   echo "|	 II  dpkg --get-selections \"*\" backed up";
-  sudo tar -czf $DATE-$HOST-pkgstates.tar.gz /var/lib/aptitude/pkgstates;
+  sudo tar -czf "$DATE"-"$HOST"-pkgstates.tar.gz /var/lib/aptitude/pkgstates;
   echo "|  II  aptitude package states backed up";
-  sudo tar -czf $DATE-$HOST-backups.tgz $DATE-$HOST*.tar.gz;
+  sudo tar -czf "$DATE"-"$HOST"-backups.tgz "$DATE"-"$HOST"*.tar.gz;
   echo "|	 II  gzip'd tarball made of individual files";
 # Remove temporary tar.gz files
   echo "|  II  Removing temporary .tar.gz files";
-  sudo rm -f $DATE-$HOST-*.tar.gz;
+  sudo rm -f "$DATE"-"$HOST"-*.tar.gz;
 # Set permissions to user:group as defined near top of file
-  sudo chown $CHOWNER $DEST -R;
+  sudo chown "$CHOWNER" "$DEST" -R;
   echo "| ";
   echo "|  II  Ownership of:";
   echo "|  II  $DEST:";
@@ -69,5 +76,5 @@ echo "|	 II  Changed current directory to $DEST";
   echo "|  !!  Execution complete...";
   echo "+===============================================+";
 # Return from whence we came
-  cd $DIR;
+  cd "$DIR" || echo "Couldn't cd to $DIR! Quitting..." && exit;
   echo "";
